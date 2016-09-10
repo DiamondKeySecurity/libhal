8677e58ebc751ab47f1a14273b7ef19dfa17a293
Rob Austein <sra@hactrn.net>
2016-09-09 20:35:52 -0400

    Fencepost error in ks_find().

--- ks_index.c
+++ ks_index.c
@@ -56,7 +56,7 @@ static int ks_find(const hal_ks_index_t * const ksi,
   int hi = ksi->used;
 
   for (;;) {
-    int m = (lo + hi) / 2;
+    int m = (lo + hi) >> 1;
     if (m == lo) {
       *where = hi;
       return 0;
