4eebeee153bfc52d562f1f01599111a67ddb7efd
Paul Selkirk <paul@psgd.org>
2018-10-25 17:18:43 -0400

    Enable bloat tests, since they work with the current 8K keystore block size.

--- unit-tests.py
+++ unit-tests.py
@@ -1272,40 +1272,32 @@ class TestPKeyAttribute(TestCaseLoggedIn):
                                           for j in xrange(n_attrs)))
                     pinwheel()
 
-    # These sizes work with a 4096-byte keystore block; if you tweak
-    # the undelrying block size, you may need to tweak these tests too.
+    # These sizes work with a 8192-byte keystore block; if you tweak
+    # the underlying block size, you may need to tweak these tests too.
 
     def test_attribute_svelt_volatile_many(self):
         self.load_and_fill(0, n_attrs = 64)
 
     def test_attribute_bloat_volatile_many(self):
-        self.skipUnlessAll("bloat tests with large flash blocks exceed XDR limits, sigh")
-        with self.assertRaises(HAL_ERROR_RESULT_TOO_LONG):
-            self.load_and_fill(0, n_attrs = 128)
+        self.load_and_fill(0, n_attrs = 128)
 
     def test_attribute_svelt_volatile_big(self):
         self.load_and_fill(0, n_attrs = 6, n_fill = 256)
 
     def test_attribute_bloat_volatile_big(self):
-        self.skipUnlessAll("bloat tests with large flash blocks exceed XDR limits, sigh")
-        with self.assertRaises(HAL_ERROR_RESULT_TOO_LONG):
-            self.load_and_fill(0, n_attrs = 6, n_fill = 512)
+        self.load_and_fill(0, n_attrs = 6, n_fill = 512)
 
     def test_attribute_svelt_token_many(self):
         self.load_and_fill(HAL_KEY_FLAG_TOKEN, n_attrs = 64)
 
     def test_attribute_bloat_token_many(self):
-        self.skipUnlessAll("bloat tests with large flash blocks exceed XDR limits, sigh")
-        with self.assertRaises(HAL_ERROR_RESULT_TOO_LONG):
-            self.load_and_fill(HAL_KEY_FLAG_TOKEN, n_attrs = 128)
+        self.load_and_fill(HAL_KEY_FLAG_TOKEN, n_attrs = 128)
 
     def test_attribute_svelt_token_big(self):
         self.load_and_fill(HAL_KEY_FLAG_TOKEN, n_attrs = 6, n_fill = 256)
 
     def test_attribute_bloat_token_big(self):
-        self.skipUnlessAll("bloat tests with large flash blocks exceed XDR limits, sigh")
-        with self.assertRaises(HAL_ERROR_RESULT_TOO_LONG):
-            self.load_and_fill(HAL_KEY_FLAG_TOKEN, n_attrs = 6, n_fill = 512)
+        self.load_and_fill(HAL_KEY_FLAG_TOKEN, n_attrs = 6, n_fill = 512)
 
 
 @unittest.skipUnless(ecdsa_loaded, "Requires Python ECDSA package")
