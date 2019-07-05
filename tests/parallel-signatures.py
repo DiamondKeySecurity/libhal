38f768f3cccf6a473f26944bdf48d33c6ff15229
emilio.lopez@dkey.org <emilio.lopez@dkey.org>
2019-07-04 20:07:59 -0400

    Get basic ECDSA signing and verification working

--- tests/parallel-signatures.py
+++ tests/parallel-signatures.py
@@ -56,13 +56,27 @@ from tornado.ioloop     import IOLoop
 from tornado.iostream   import IOStream, StreamClosedError
 
 from Crypto.Util.asn1               import DerSequence, DerNull, DerOctetString
+#---------------------------------------------------------------------------
 from Crypto.Util.number             import inverse
 from Crypto.PublicKey               import RSA
+from Crypto.Cipher                  import AES
 from Crypto.Cipher.PKCS1_v1_5       import PKCS115_Cipher
 from Crypto.Signature.PKCS1_v1_5    import PKCS115_SigScheme
 from Crypto.Hash.SHA256             import SHA256Hash as SHA256
 from Crypto.Hash.SHA384             import SHA384Hash as SHA384
 from Crypto.Hash.SHA512             import SHA512Hash as SHA512
+
+from ecdsa                          import der as ECDSA_DER
+from ecdsa.keys                     import SigningKey as ECDSA_SigningKey
+from ecdsa.keys                     import VerifyingKey as ECDSA_VerifyingKey
+from ecdsa.ellipticcurve            import Point
+from ecdsa.curves                   import NIST256p, NIST384p, NIST521p
+from ecdsa.curves                   import find_curve as ECDSA_find_curve
+from ecdsa.util                     import oid_ecPublicKey
+
+#from hashlib                    import sha256 as SHA256, sha384 as SHA384, sha512 as SHA512
+#--------------------------------------------------------------------------------
+
 
 try:
     import statistics
@@ -98,8 +112,12 @@ class PKey(cryptech.libhal.Handle):
         self.deleted = True
 
     @coroutine
-    def sign(self, data, length = 1024):
-        r = yield self.hsm.pkey_sign(self, data = data, length = length)
+    def key_type(self):
+        r = yield self.hsm.pkey_get_key_type(self)
+        raise Return(r)
+    @coroutine
+    def sign(self, hash = 0, data = "", length = 1024):
+        r = yield self.hsm.pkey_sign(self, hash = hash, data = data, length = length)
         raise Return(r)
 
     @coroutine
@@ -191,8 +209,9 @@ class HSM(cryptech.libhal.HSM):
             pass
 
     @coroutine
-    def pkey_sign(self, pkey, data, length = 1024):
-        with (yield self.rpc(RPC_FUNC_PKEY_SIGN, pkey, 0, data, length)) as r:
+    def pkey_sign(self, pkey, hash = 0, data = "", length = 1024):
+        hash, data = 0, hash.finalize_padded(pkey)
+        with (yield self.rpc(RPC_FUNC_PKEY_SIGN, pkey, hash, data, length)) as r:
             raise Return(r.unpack_bytes())
 
 
@@ -207,10 +226,10 @@ def client(args, k, p, q, r, m, v, h):
         n = q.pop(0)
         logger.debug("Signing %s", n)
         t0 = datetime.datetime.now()
-        s  = yield p.sign(data = m)
+        s  = yield p.sign(hash = h)
         t1 = datetime.datetime.now()
         logger.debug("Signature %s: %s", n, ":".join("{:02x}".format(ord(b)) for b in s))
-        if args.verify and not v.verify(h, s):
+        if args.verify and not v.verify(args.text, SHA256, s):
             raise RuntimeError("RSA verification failed")
         r.add(t0, t1)
 
@@ -233,17 +252,39 @@ def main():
         logging.getLogger().setLevel(logging.DEBUG)
 
     k = key_table[args.key]
-    d = k.exportKey(format = "DER", pkcs = 8)
-    h = SHA256(args.text)
-    v = PKCS115_SigScheme(k)
-    q = range(args.iterations)
-    m = pkcs1_hash_and_pad(args.text)
-    r = Result(args, args.key)
+    key_type = args.key.split('_')[0]
 
+    if key_type == "rsa":
+        d = k.export_key(format = "DER", pkcs = 8)
+        v = pkcs1_15.new(k)
+        h = SHA256(args.text)
+    else:
+        sk = PreloadedKey.db[HAL_KEY_TYPE_EC_PRIVATE, HAL_CURVE_P256]
+        vk = PreloadedKey.db[HAL_KEY_TYPE_EC_PUBLIC, HAL_CURVE_P256]
+        d = sk.der
+        v = vk
+        #k1 = hsm.pkey_load(sk.der, HAL_KEY_FLAG_USAGE_DIGITALSIGNATURE)
+        #k2 = hsm.pkey_load(vk.der, HAL_KEY_FLAG_USAGE_DIGITALSIGNATURE)
+
+        #h = SHA256(args.text)
+        #sig1 = yield k1.sign(hash = h)
+        #sig2 = sk.sign(hamster, SHA256)
+        #k1.verify(signature = sig2, hash = h)
+        #k2.verify(signature = sig2, hash = h)
+        #vk.verify(hamster, SHA256, sig1)
+        #return
+
+    m = pkcs1_hash_and_pad(args.text)
+    q = range(args.iterations)
+    r = Result(args, args.key)
     hsms = [HSM() for i in xrange(args.clients)]
 
     for hsm in hsms:
         yield hsm.login(HAL_USER_NORMAL, args.pin)
+
+    for hsm in hsms:
+        h = hsm.hash_initialize(HAL_DIGEST_ALGORITHM_SHA256, mixed_mode = True)
+        h.update(args.text)
 
     pkeys = yield [hsm.pkey_load(d, HAL_KEY_FLAG_USAGE_DIGITALSIGNATURE) for hsm in hsms]
 
@@ -440,6 +481,242 @@ sQLesqTufT1mVZd/ndLyvjDJjNKUE0w1g/1xNtg6N5aM+7pc/DwE/s+EtCxc/858
 dQYLBHIPcw6e0FdL3nTs44BpAqcK28N5eWbe/KaZ3EA0lHRmyOQ++WgU6jo=
 -----END RSA PRIVATE KEY-----
 '''))
+
+key_table.update(ecc_256 = '''-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIDLIUDl4ZL62cMVoDekHxuUKHQUMUMXvh2tJTGj4vPmwoAoGCCqGSM49
+AwEHoUQDQgAEX8IPZV1M/i6OYZg2LCxwgHQtMrIt57YqMrTEgI+Db3hz1Tdpsbv1
+v7Yiq/Qd6uJ0E14f/mir94tTw1ezl/CzBg==
+-----END EC PRIVATE KEY-----''')
+
+key_table.update(ecc_384 = '''-----BEGIN EC PRIVATE KEY-----
+MIGkAgEBBDCSIxIaAYAEMtHiirDlGtihnUSRjAr2enqW24x8JXQLqmY4glBFxpy4
+bls3mJ/C0FagBwYFK4EEACKhZANiAARnEiGXGAdjWqUVgvMwDpTYpQRuFpMbVuHA
+TjC0LjsLMQ+IpdJUMBSys4lp82AJOg4+pmxRsbsSO0IiaMFcS3FzGc6KBvFNXqfh
+HkkzWsLk/S5G6FG3ZF1JaSMpoj2FtAQ=
+-----END EC PRIVATE KEY-----''')
+
+key_table.update(ecc_521 = '''-----BEGIN EC PRIVATE KEY-----
+MIHcAgEBBEIBXGqrYLTV96IGy84CzhCl/DUzZ3AxqD2dIZ6e80FSdTKK+A92d6KF
+xuQP/8KpRnSo35Q2mH6W+MwODayW2WpI22egBwYFK4EEACOhgYkDgYYABAB3e4ux
+kgEyL6Ib9hgewTSs1AK0/hQqT6AOVySY6tLl9PxI1Lwx2zMniNq2AcjUaF3+AVDX
+ayCMXD1lQY7FpPvqQQAe1RFfobtvCgddV+3L573bebeiabh3xEIHtTT7fo2rXUZ2
+0cdSS0nkUiSo2hXrEUJf6yN5x0+RB8YtfqEtvzDzOA==
+-----END EC PRIVATE KEY-----''')
+
+class PreloadedKey(object):
+    """
+    Keys for preload tests, here at the end because they're large.
+    These are now in PKCS #8 format, which gives us a single,
+    consistent, self-identifying private key format.  See tools
+    like "openssl pkcs8" if you need to convert from some other format
+    (eg, PKCS #1 or secg).
+    """
+
+    db = {}
+
+    def __init__(self, keytype, fn2, obj, der, keylen = None, curve = HAL_CURVE_NONE):
+        self.keytype = keytype
+        self.fn2     = fn2
+        self.obj     = obj
+        self.der     = der
+        self.keylen  = keylen
+        self.curve   = curve
+        self.db[keytype, fn2] = self
+
+class PreloadedRSAKey(PreloadedKey):
+
+    @classmethod
+    def importKey(cls, keylen, pem):
+        k1 = RSA.importKey(pem)
+        k2 = k1.publickey()
+        cls(HAL_KEY_TYPE_RSA_PRIVATE, keylen,
+            k1, k1.exportKey(format = "DER", pkcs = 8), keylen = keylen)
+        cls(HAL_KEY_TYPE_RSA_PUBLIC,  keylen,
+            k2, k2.exportKey(format = "DER"          ), keylen = keylen)
+
+    def sign(self, text, hash):
+        return PKCS115_SigScheme(self.obj).sign(hash(text))
+
+    def verify(self, text, hash, signature):
+        return PKCS115_SigScheme(self.obj).verify(hash(text), signature)
+
+class PreloadedECKey(PreloadedKey):
+
+    @staticmethod
+    def _check(condition):
+        if not condition:
+            raise ECDSA_DER.UnexpectedDER()
+
+    @classmethod
+    def importKey(cls, curve, pem):
+        der = ECDSA_DER.unpem(pem)
+        car, cdr = ECDSA_DER.remove_sequence(der)
+        cls._check(cdr == "")
+        version, cdr = ECDSA_DER.remove_integer(car)
+        cls._check(version == 0)
+        algid, pkinfo = ECDSA_DER.remove_sequence(cdr)
+        oid, cdr = ECDSA_DER.remove_object(algid)
+        cls._check(oid == oid_ecPublicKey)
+        oid, cdr = ECDSA_DER.remove_object(cdr)
+        sk_curve = ECDSA_find_curve(oid)
+        cls._check(cdr == "")
+        car, cdr = ECDSA_DER.remove_octet_string(pkinfo)
+        cls._check(cdr == "")
+        car, cdr = ECDSA_DER.remove_sequence(car)
+        cls._check(cdr == "")
+        version, cdr = ECDSA_DER.remove_integer(car)
+        cls._check(version == 1)
+        privkey, cdr = ECDSA_DER.remove_octet_string(cdr)
+        k1 = ECDSA_SigningKey.from_string(privkey, sk_curve)
+        k2 = k1.get_verifying_key()
+        cls(HAL_KEY_TYPE_EC_PRIVATE, curve, k1, der,         curve = curve)
+        cls(HAL_KEY_TYPE_EC_PUBLIC,  curve, k2, k2.to_der(), curve = curve)
+
+    def sign(self, text, hash):
+        return self.obj.sign(text, hashfunc = hash)
+
+    def verify(self, text, hash, signature):
+        return self.obj.verify(signature, text, hashfunc = hash)
+
+
+# openssl genrsa 1024
+PreloadedRSAKey.importKey(1024, '''\
+-----BEGIN PRIVATE KEY-----
+MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAL3lCUM6+VCF0J79
+rt4ihf1IY9V9d8Dz/raKWcX41zAuuHGSvZHgRVAHrn70BvGozweqiXp+FnOYOzjx
+yXa04UYmkTyIyGIWbiLpIaXZFdhobTGL6UkWtdcIsVCi8gQy66QETXb3CtrXEgHv
+a1hHXiW7xERf4yGlu7PjgzWOUKuVAgMBAAECgYB5T3byXxtkiIf0+eUbfXyRiQxW
+F3hIaNdAr/OX2FdILaCg1XiLV3WQIcgkzCofeZMAt75p5WDcadTzlOr6DaWXOiCj
+JeXyr3TYpu8iasz9dJuy573+iPQ5KDD6MoF9oXNfoXEC01XmmZT1CLBq4EiX/fI8
+IX2J+KbnOD6lYKotoQJBAPn1n9HpOtMfGnrDa8fgInI/ko5Fx/54xrnRICuIxUFe
+wnOwGIstXEohl9ZtCf2DK0molkqYgd7NlCZ3Jqbzh+kCQQDCe9IXbtGHEtcTTfv6
+3uTkxm2LoF81N5QGvB9+156kvqprH0cr6H0hBnW980iqNmbyLuRBDLLgvy1GeTya
+NmbNAkEAhTB9dZOKVb7IFEwXHUzv7eK0C/1g4NaoRZEKTEg3m2qLwKs/mMGV4KZf
+ytEVNrFzGm+rjZoP8ZGndIue2+z+KQJAJKAY4pzWDK/5nQMUrxwG0yajPqZHB8id
+sd7/t213zOKzSVUsnBI+ble/GLSWPKfeH0HBbparoTOfnSP7y7bvCQJBAJwgUTTU
+EMB20Ue1MndpoQz0h0wJitwnQj9jQ1MTYzqWxNKI/JCNzh1BAnEV269YcvHbxvH6
+XsVNq/sVoqe0Img=
+-----END PRIVATE KEY-----
+''')
+
+# openssl genrsa 2048
+PreloadedRSAKey.importKey(2048, '''\
+-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCxu+rqzKENcP84
+xUC4ugxx4ZAtQiJ7N9KFWoV/IyoAOOqEVFZZuiefRdX1mTKlTghv2fo+0sBM1p5+
+GmxSUjiz7bRhOVDsaLAn2n3v83WjW7TrSNrgt2qF9i2fwZlf4kB9rk2qmYsoInnx
+cnerbww8tHBinnIpySOTNyFkcXKOzVbuvlol94n++xC6xfeSWYJ6EnWPFIc+Hyjq
+VOMUycEry3frRhO7RQYr9FRwQBg7uXsVaN3w+Nl9rBcis4CEIuRW8MV+myXajIUg
+v13VATztZ3LJlcC1YuzraCdf6SIGr2BSG7kayBxCJ8cFxGIZl8fRNEgyAixKOyiv
+Meon26hDAgMBAAECggEAarPHn/kDw18QSlPZvE0txXzGovUuYE0uHRMwTBifUYzZ
+9mHviWTB/tPdVNoJVsaXOBdkLi02d6rtcgWI/I8SLatKfIf9dkCPaQ26R8Eg4dbf
+sdWGWhyjXhNiZqPC4ZaiYxGu3PnbWlgAX5z6DTjY3uTxB7PaFA4GRo51hKi7PtE1
+0lQZShxGFxKSH5BlMHC1DGuS6GEc37OQiuL5DT2EXaECWIeuHqajDxyLSBGxednO
+x0l9NfwJqwdZMiWXiB+u9adRjuJioHrKdFRg37siBBcxQUvOIiUfWo34OURh3Oey
+AA9JLcl1NaHRy+OI7zBMUCc4h2yP4GkqgME8rbPMOQKBgQDdM/yHgOz/LW0OKMTd
+JquuDS9a5j3uwmYCVrioUcfiGxxnzg0PNMwj+iEs8mCl5HYxoGpmZWT1QWt3et23
+f+VfqRI7sp99r2tWB84HgKoniOiyjePSpjxX+JUFwNwPWHhpcJFfa0wkgJROBVT8
+3z/bAEQSVw5SNkcaVweZsed2VQKBgQDNsWa20+4getgeSY6b8ONgHrn5YPyB88x1
+5RufH7Ii3nHCKYhurWpbOXj76ChMADJpT/uoz0h+95G57QHvKOc9rx+M34JxcwYz
+prcEip4i/WJx+CMfGW3sP7rgWkQ1Zce8UTQyFGWIg1+RMeb5Tj7WzTTjlr5st1Nt
+9WXLWABMNwKBgQDI5qhaYUveTuGXVIvH15FhWPRDeI2TRdfkE+NObEhAISNhXBEc
+WHV3z3tGGTSSNrbqR6mbKkfRQANc1qh8OpqWkTXQJmEYg7PWxOvogIEJuxys3pWm
+AfkYZNrkCM9U2KM0EFrX8NmM5YLMgac4q4KRYuJwTcTiAg397EEo5B02qQKBgQCM
+Dk8yDfwGgZ7GgK9p3wvzVviqpS9KDQca/3VfU+WneG9dFYqgUdQpaWjHnbxSg4/P
+xXZF614hldDUGF1Iy0Eo7yU8drQLK5b9dHcJ0Jj8mit+DsdbVaVTR9GnEc9/zF3E
+DEbOErVPzqQz6RasimqfN/rbPLlMrUdRh9ZYLwOE4QKBgHNTyEgDQ9TtpE9SzSj0
+w71dy9U+91fpQ4p01v0KcEy51L9yEoclRg93R8UCJVpFfQkxDRFKt6fEKp9ZW1bd
+PfAVg0cH8gVEhRPcjjjDBclM056iLcUNMzERB9zRQWgUfJzeZgQpHjMeF6pAbk4U
+E3z0uobdL47r29zWrAK80ov5
+-----END PRIVATE KEY-----
+''')
+
+# openssl genrsa 4096
+PreloadedRSAKey.importKey(4096, '''\
+-----BEGIN PRIVATE KEY-----
+MIIJQgIBADANBgkqhkiG9w0BAQEFAASCCSwwggkoAgEAAoICAQDNalhJC34OtQ0j
+4QlP+Sj3bAzY858Weabcpzfiea4rqx7PQMmv1WJv1HkrjpE2CpxR0Ch2NTpzLf8x
+kDndKo/tHBH8tC1aYrZr5PiJ7La9BnSIIMDY0jiCOCYICNmc4P+bnJReMKKD/Any
+kZeiWDygee914uDWOgg5IX0650usjpomcOA5916DOYJZeZaXtNeuodUhZmFlkXup
+BlGyjplDX/yaOm9UPR2sBIHEycjO+l7jKetjY0HzTtf8PlttJynb3ZkpM1/m6c83
+VKwXLnKbN8tmt82Sxh5/rE9dxWvo6I5uLj0N7x7XVo0+Pp+AvPOoC7WuOOsOSC9f
+HkbuA8lerVqrq7EHZwoytFL6K+bILy9CXM+VDBbbShh5458KDt100Aly7eM1O40l
+pl2Fz2zOlWFXwNdmhd79HNQ2NkfvBSq54fJ+lsvBL6HcxwmqttYV0JAGK9megjTl
+wXYkgcR5HdLQbKjb8iF414VfZO3ygOcdjhqiVEeJHkIx0zxSgZLFH1jXZ9n11yp0
+Ifd3rAMCwEMdMITxkjAXCFbqID6DnhgTdfsGWyBavrSmgIOAf4ImOHRUNXGovuv7
+DvqwP1YBAxrsZv4ynZcJichk9dOuybBVtn2zm8VdQQ4NiibqoGVtkHyDJnN57lKR
+zWPHZ+earFG1+UDhbfFUBAWh/asWJQIDAQABAoICAHGFBclIWp/SGrLrWHQj5Cno
+Qqla6V5IWJi0qMO2LzTe2qX6zfsoV8ApkdUqcsGE16K3AMVi4SxqDQ/c/r1lGikV
+TDN1d2LuDpZN8bR3Zv3LpXbdcvXgJ25/jTT/fxOdnGTpAW2UPxvlm7G8GCqVzNt6
+x4VEnrCFXaoFAeodjMqN/E6mLqSaOlcW3xyHf1vTiyZtNRXCOK0i204rudaNy9Fa
+KIUrKf9Jzy4TR4vzX9BSuUGdFwLUBcxqlB5Ib8pAzHc/XbdRNvyr1eJkHr6o4pk5
+GmVnymoEpzIzjaDqy5npe10UnJUowsiKgoAVp5xDz/vcjMj9l5lK9zL/zn8XBJ8a
+McPsLu8v2R+Dvuej4i8diUFiIiq1N12lAfksYkAmc4a2TTjv86NOYvxDLKW8MC/y
+MF5ll1s5fsv7yOTkNseZ9RwOO8FL2D4Vt+SMO6umGunV0ef9iBoDbs1mzztM9ZfV
+pU+I6VcSE71IceKIoIcOkHNXWBb96KGjfez64SvntmhqTOic3/M1vsqodQ79kbAy
+D5X1tUiBOGpt2CbE3PskXPXgI3v3I+ZblBPU/W/trrFZplMAt7Yca/LQH6JHOEul
+rLW0jg7+iY+P29TNUnAYfO1W3RF11yYD3BVjeqCVE2X5i82WboFSfwrA8iqSLwmi
+KgDwnURDmbP8rR9uvNw5AoIBAQDxMKqrSluZ/G2x/jbOGxJTDVV1wIgbQhhxJbEi
+IJ+6OvIBBfW2ay1l5FyGXKReZ074kNym8zIEoEPvXzwkRaMJfUhFG0fFMnlYb6kT
+yw08s0vx5lVn4IRs2NWTh21YYvDI2b6tCQw2Y6Wb7tkd17MFeWNk56plt2xNINBw
+ZgbwnGW8ukA3rSmMga9N1Ot2eSwyEHg8r6Xsq3IRUNZdT9VPqSqE/dGLV2ge//zU
+GmWB+MQkzxnGZqx8/hKUnWiUqqwcwVKtyWNkIB6YoR7FXLODxweb2CGPowtBl7eC
+z3hXqHwjaJ+sEE0cytmBiDYNvQZ7Jiuv6RkYONPQNmnSODJ/AoIBAQDaB1NyWat8
+BT/rvOI4luOQyB+EF214TUFskM1RxrTMHqom7gHA2C3RzEcxuW2ATb/yFY/3kchp
+B2HWYiDBIr3czFFtoC8BB8Xea4nG8xHkyu4N6MT4gQr15l62CQ0cZbJ+IXMFYd1r
+9n09CSpi1F+inmoUFUA9nhjNHmFPSPrh4NA3/P0LRkHpEv0RxcmARiAYjfA/xCyv
+DclEFA3RIJVGasQmbhYhez4IQc6Tnr9Vs8y/za7BsdWCFLRT1sRm82h6rtlCNLa1
+FddEl46BDII29jLrvRPQq9BmgNvn0RsQNLN5BlLUnusMQEdb5XBFIkDyegwSBl0S
+/wDFsQXYJ11bAoIBAAzAfvWJe+hbUU1s3GbvRXvAo7kTEd8nEnXBAWmFggxtm2f0
+cbZQWlkdxsqP0VzwZ1t7BT+KciWOd6kLRvm/+MvCpPSKabQCZCoJ1U2nma6Q6D4E
+6JUSaM38dOZU7bIToRltcchiRxQFFrd03GPYiK2ManXmTbsNBJvbVUnsCwfjrPla
+e5S2AhFctfEOhdiAtZZ8Cm9i9gnaUrCZrovGccfBcO8RIGgWmfmxnSN1tdI2XeB4
+VquHuCzvnfnfjKz3jRwEGKP5ltivXg7BzgevTAp2lm9l8PhQLYMtS1WONJWl4dJZ
+cL+/KOSR0QgpNHpreRE/xmWJlHsUXhaPGe80a28CggEAS7mDqBGw17BTFSNTRuhR
+PXXkTFJ+uOvML1LTnPVw0fxMiJzdiwcKUvWqPID0oT1fz0rh59CAVMo1luoaCjpw
+ywFDvtmFucvjU27yOWEu89HvgHE6CD15exJ6DqtyHEzOA7TGaDYnW1oPAWXvjMfK
+N4bhvPvshfolDwjgL1Nfn5gfeXKqyIGsXrEzqDqT07OHbj/OE7ek9V/KaZS1f87b
+ScvHOLm/gf11/BQvFWJrfe6mzG85pRiI2VcrknrjWAZuYT3dQ3tfgeGGFNK2seeQ
+SlqUMQBIo48pIJDcz9T2m1hShVRG7IYNiQHCRw6XQcu11wHAs3n2VaSvtfnX181E
+RQKCAQEAyTf2T6yEPYNby5sT8uCh+ivB9PbnnwU/Gln6kbAwcvdmcu5C6JE7I/JB
+mP9maYsBrZM+vBL+yyWfjpjYp6x6NYr/QerxJoUuE6NpwXy7kg44oFm/H4c5Ppy1
+E0FXLO30DzC7QMTqfNzkdCmb1Mxes4u5nQu7bbIWKTCgiBilTqEF1uDWaeKtlL9n
+KolX2BYHpljJQBQZ2AGovp65IACoGn3erPyxAt6ypO59PWZVl3+d0vK+MMmM0pQT
+TDWD/XE22Do3loz7ulz8PAT+z4S0LFz/znx1BgsEcg9zDp7QV0vedOzjgGkCpwrb
+w3l5Zt78ppncQDSUdGbI5D75aBTqOg==
+-----END PRIVATE KEY-----
+''')
+
+# openssl ecparam -genkey -name prime256v1 | openssl ec
+PreloadedECKey.importKey(HAL_CURVE_P256, '''\
+-----BEGIN PRIVATE KEY-----
+MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQg8GtWEPWIUXZ7zA+Q
+9/g3XTfyJHzE+CS78JU5PyVxLO2hRANCAATeYHkGY3kVrh9XvguwNYikFTGS0+pg
+YnpY4GuM+zUdaQ1Tskc7epj3LL4JcVI9vz0ZZCBvnAu00+fhBgyP4pFM
+-----END PRIVATE KEY-----
+''')
+
+# openssl ecparam -genkey -name secp384r1 | openssl ec
+PreloadedECKey.importKey(HAL_CURVE_P384, '''\
+-----BEGIN PRIVATE KEY-----
+MIG2AgEAMBAGByqGSM49AgEGBSuBBAAiBIGeMIGbAgEBBDCVGo35Hbrf3Iys7mWR
+Im5yjg+6vPIgzbp2jCbDyszBo+wTxmQambG4g8yocp4wM6+hZANiAATYwa+M8T8j
+sNHKmMZTvPPflUIfrjuZZo1D3kkkmN4r6cTNctjaeRdAfD0X40l4yPnGIP9ParuK
+VVl1y0TdQ7BS3g/Gj/LP33HDESP8gFDIKFCWSDX0uhmy+HsGsPwgNoY=
+-----END PRIVATE KEY-----
+''')
+
+# openssl ecparam -genkey -name secp521r1 | openssl ec
+PreloadedECKey.importKey(HAL_CURVE_P521, '''\
+-----BEGIN PRIVATE KEY-----
+MIHuAgEAMBAGByqGSM49AgEGBSuBBAAjBIHWMIHTAgEBBEIBtf+LKhJNQEJRFQ2c
+GQPcviwfp9IKSnD5EFTqAPtv7/+t2FtmdHHP/fWIlZ7jcC5N9dWy6bKGu3+FqwgZ
+AYLpsqihgYkDgYYABADdfcUeP0oAZQ5308v5ijcg4hePvhvVi+QKcrwmE9kirXCF
+oYN1tzPmXZmw8lNJenrbwaNzopJR84LBHnomGPogAQGF0aRk0jE8w1j1oMfrrzV6
+vCWnkh7pyzsDnrLU1HrkWeqwihzwMzYJgFzToDH+fCh7nrBFZZZ9P9gPYMlSM5UM
+eA==
+-----END PRIVATE KEY-----
+''')
 
 if __name__ == "__main__":
     IOLoop.current().run_sync(main)
